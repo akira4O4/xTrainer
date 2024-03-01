ff40dcb6e70e5c4ed3977fc7988660ed27e17ff8
akira.404 <akira.404@outlook.com>
2024-03-01 16:25:52 +0800

    test

--- train.py
+++ train.py
@@ -20,8 +20,8 @@ if __name__ == '__main__':
                         # default=r"configs/hrsf_zuo.yml",
                         # default=r"configs/danyang_C2.yml",
                         # default=r"configs/danyang_E_mt.yml",
-                        default=r"configs/danyang_G.yml",
-                        # default=r"configs/danyang_F_seg_exp2.yml",
+                        # default=r"configs/danyang_G.yml",
+                        default=r"configs/danyang_F_seg_exp2.yml",
                         # default=r"configs/danyang_E_mt_exp2.yml",
                         help='your project config json.')
     args = parser.parse_args()
