bff511f443796b2a7d24b0b1c0dce95d5e731b92
akira.404 <akira.404@outlook.com>
2024-07-08 01:16:01 +0800

    Update

---
+++ src/config.py
@@ -0,0 +1,44 @@
+import os
+from typing import Optional, Any, Union
+from utils import load_yaml, load_json
+
+
+class Config:
+    def __init__(self, path: Optional[str] = None) -> None:
+
+        self._path: str = ''
+        self._metadata = None
+
+        if path is not None:
+            if os.path.exists(path):
+                self._path = path
+                self.load()
+
+    def __call__(self, key: Union[str, int]) -> Any:
+        if isinstance(self._metadata, list):
+            return self._metadata[key]
+
+        elif isinstance(self._metadata, dict):
+            return self._metadata.get(key, None)
+
+    @property
+    def metadata(self):
+        return self._metadata
+
+    def set_metadata(self, val) -> None:
+        self._metadata = val
+
+    def set_path(self, path: str) -> None:
+        self._path = path
+
+    def load(self) -> None:
+        basename = os.path.basename(self._path)
+        name, subfix = os.path.splitext(basename)
+        if subfix == '.yaml' or subfix == '.yml':
+            self._metadata = load_yaml(self._path)
+        elif subfix == '.json':
+            self._metadata = load_json(self._path)
+
+
+if __name__ == '__main__':
+    ...
---
+++ src/torch_utils.py
@@ -0,0 +1,38 @@
+import torch
+
+
+def iou(pred: torch.Tensor, target: torch.Tensor):
+    b = pred.shape[0]
+    IoU = 0.0
+    for i in range(0, b):
+        # compute the IoU of the foreground
+        Iand1 = torch.sum(target[i, :, :, :] * pred[i, :, :, :])
+        Ior1 = torch.sum(target[i, :, :, :]) + torch.sum(pred[i, :, :, :]) - Iand1
+        IoU1 = Iand1 / Ior1
+
+        # IoU loss is (1-IoU1)
+        IoU = IoU + (1 - IoU1)
+
+    return IoU / b
+
+
+class IOU(torch.nn.Module):
+    def __init__(self):
+        super(IOU, self).__init__()
+
+    def forward(self, pred: torch.Tensor, target: torch.Tensor):
+        return self._iou(pred, target)
+
+    def _iou(self, pred: torch.Tensor, target: torch.Tensor):
+        b = pred.shape[0]
+        IoU = 0.0
+        for i in range(0, b):
+            # compute the IoU of the foreground
+            Iand1 = torch.sum(target[i, :, :, :] * pred[i, :, :, :])
+            Ior1 = torch.sum(target[i, :, :, :]) + torch.sum(pred[i, :, :, :]) - Iand1
+            IoU1 = Iand1 / Ior1
+
+            # IoU loss is (1-IoU1)
+            IoU = IoU + (1 - IoU1)
+
+        return IoU / b
